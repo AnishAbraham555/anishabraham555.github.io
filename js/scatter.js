32ce88081b04197734eb55e94ee8d4b16b22bb37
AnishAbraham555 <36295700+AnishAbraham555@users.noreply.github.com>
2019-07-27 00:51:11 -0700

    Update scatter.js

--- js/scatter.js
+++ js/scatter.js
@@ -155,7 +155,6 @@ d3.csv("data/Global Superstore.csv", function(dataread){
 	// read data from the slider when it slides
 	d3.select("#month_slider")
 	        .append("g")
-		.append("text")
        		.text("Month Slider");
 		.on("input", function () {
 			month_index = Number(this.value);
