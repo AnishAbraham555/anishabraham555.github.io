edc5334b29215bee187c71d5c6cd03bc934f5187
AnishAbraham555 <36295700+AnishAbraham555@users.noreply.github.com>
2019-08-03 11:49:39 -0700

    Update worldmap.js

--- js/worldmap.js
+++ js/worldmap.js
@@ -40,7 +40,7 @@ d3.csv("data/Global Superstore.csv", function(dataread) {
 
         // set the size and create svg
         var width = 1050;
-        var height = 300;
+        var height = 200;
         var svg = d3.select("body")
                     .append("svg")
                     .attr("width", width)
